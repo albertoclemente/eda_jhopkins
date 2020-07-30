6b4690b1414ff4d9c8ae8c592c5a9d41b7184d26
albertoclemente <clemalb@gmail.com>
2020-07-30 20:00:42 +0200

    state names

--- first_look.R
+++ first_look.R
@@ -12,3 +12,12 @@ names(ozone)
 
 #variables classes
 str(ozone)
+
+#How many States?
+ozone %>% 
+        select(State.Name) %>% 
+        unique %>% 
+        nrow
+#Why are there 53 States? Check the names
+
+unique(ozone$State.Name)
