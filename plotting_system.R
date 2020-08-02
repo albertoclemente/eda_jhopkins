a438a4a509f575b9fa6b4d2f8730bd785a6e8a8c
albertoclemente <clemalb@gmail.com>
2020-08-02 18:14:26 +0200

    plotting systems

--- plotting_system.R
+++ plotting_system.R
@@ -13,11 +13,16 @@ with(airquality, {
 with(cars,plot(speed,dist))
 
 #Lattice system
-library(lattice
-)
-
-#Lattice library
+library(lattice)
 
 state <- data.frame(state.x77, region = state.region )
 
 xyplot(Life.Exp ~ Income | region, state, c(4,1))
+
+
+#ggplot2
+
+library(ggplot2)
+data(mpg)
+
+qplot(displ,hwy,data =mpg)
