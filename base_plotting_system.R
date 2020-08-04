de88dc600b176af1c2f3ed30a116d4d48b9bde10
albertoclemente <clemalb@gmail.com>
2020-08-04 18:36:44 +0200

    base plotting system

--- base_plotting_system.R
+++ base_plotting_system.R
@@ -1,5 +1,19 @@
 
 # boxplot -----------------------------------------------------------------
-
 airquality <- transform(airquality,Month =factor(Month))
 with(airquality, boxplot(Ozone ~ Month), ylab="Ozone (ppb)")
+
+# Scatterplot -------------------------------------------------------------
+with(airquality,plot(Wind,Ozone))
+
+#Subsetting 
+with(subset(airquality,Month == 5),
+    points(Wind,Ozone,col ="blue"))
+
+
+# Multiple base plots -----------------------------------------------------
+par(mfrow =c(1,2))
+with(airquality, {plot(Wind,Ozone, main="Ozone and Wind")
+        plot(Solar.R,Ozone, main ="Ozone and Solar Radiation")
+        
+})
