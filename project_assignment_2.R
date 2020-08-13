0b891f3320f244d1ad219b8229aacf14f102412d
albertoclemente <clemalb@gmail.com>
2020-08-13 10:00:46 +0200

    plot 3

--- project_assignment_2.R
+++ project_assignment_2.R
@@ -27,6 +27,8 @@ title (main= "Total amount of PM2.5 from 1999 to 2008")
 dev.copy(png,"plot1.png")
 dev.off()
 
+
+
 # 2. Have total emissions from PM2.5 decreased in the Baltimore City,
 # Maryland (fips == "24510") from 1999 to 2008? 
 # Use the base plotting system to make a plot answering this question.
@@ -54,7 +56,36 @@ dev.copy(png,"plot2.png")
 dev.off()
 
 
-        
+# 3.Of the four types of sources indicated by the 
+# (point, nonpoint, onroad, nonroad) variable, 
+# which of these four sources have seen decreases 
+# in emissions from 1999–2008 for Baltimore City? 
+# Which have seen increases in emissions from 1999–2008? 
+# Use the ggplot2 plotting system to make a plot answer this question.
+
+NEI <- readRDS("summarySCC_PM25.rds")
+library(tidyverse)
+
+pm25_Baltimore_source <- 
+        NEI %>% 
+        filter(fips==24510) %>% 
+        group_by(type,year) %>% 
+        summarize(total_pm25_source=sum(Emissions)) %>% 
+        ungroup() 
+
+plot3 <- 
+        ggplot(pm25_Baltimore_source,aes(year,total_pm25_source)) +
+        geom_point(size=2, col="blue") +
+        geom_line(col= "red") +
+        facet_wrap(. ~ type) +
+        xlab("Year")+
+        ylab("Total PM2.5 (tons)") +
+        ggtitle("Total PM2.5 in Baltimore for each source from 1999 to 2008") #non-road,nonpoint,on-road descreased while point increased
+
+with(plot3,dev.copy(png,"plot3.png", width = 480, height = 480))
+
+dev.off()
+
         
 
 
